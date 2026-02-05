2adf4a9b5209e581cb9be2da389f7c3e7a6b202c
zakikholik <garudasakti2295@gmail.com>
2026-02-05 07:36:24 +0700

    Update Schedule

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Snapshot quantity dari production_data ke production_data_hourly setiap jam pada menit 58
+Schedule::command('production:hourly-snapshot')
+    ->hourlyAt(58)
+    ->timezone(config('app.timezone', 'Asia/Jakarta'))
+    ->withoutOverlapping()
+    ->runInBackground();
 
